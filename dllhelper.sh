00363b94273e3e34684d17a4c92c729279cd35e7
tlneondo <tlneondo@gmail.com>
2024-09-30 16:22:06 -0400

    new

--- dllhelper.sh
+++ dllhelper.sh
@@ -11,4 +11,28 @@ gamefolder=$(cat ~/.steam/steam/steamapps/appmanifest_1875830.acf | grep "instal
 cd "$steamlibrarydir"
 cd "$gamefolder"
 
-ls
+folderswithexes=($(find . -name "*.exe"))
+
+
+# Print the results
+for file in "${folderswithexes[@]}"; do
+    echo "$file"
+while IFS= read -r exe_file; do
+    # Get the directory of the .exe file
+    exe_dir=$(dirname "$exe_file")
+
+    # Search for .dll files in that directory
+    dll_files=("$exe_dir"/*.dll)
+
+    # Check if any .dll files were found and print them
+    if [ -e "${dll_files[0]}" ]; then
+        echo "DLL files in directory of $exe_file:"
+        for dll in "${dll_files[@]}"; do
+            echo "$dll"
+        done
+    fi
+done < <(find "$base_directory" -type f -name "*.exe")
+
+
+
+done
